5d5db5fb0e026bf5f5a7e3bedf219055b98943d9
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2020-02-14 01:12:14 +0900

    바꿈

---
+++ JustJava/src/Ch3Ex1.java
@@ -0,0 +1,15 @@
+
+public class Ch3Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 12;
+		int num2 = 20;
+		int result = num1 + num2;
+		
+		System.out.println(num1 + " + "+num2+" = " +result);
+		System.out.printf("%d + %d = %d",num1,num2,result);
+		  //asdasd
+	}
+
+}
